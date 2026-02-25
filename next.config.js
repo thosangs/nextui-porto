c6d65fc47bb8be2763a89d2088b653cfc45e280c
Seceng <thosan.girisona@gmail.com>
2026-02-25 13:37:28 +0700

    Update wrangler.toml build command

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   images: {
+    unoptimized: true,
     remotePatterns: [
       { protocol: "https", hostname: "res.cloudinary.com", pathname: "**" },
       { protocol: "https", hostname: "images.unsplash.com", pathname: "**" },
